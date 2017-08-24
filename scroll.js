486b2bd8d573e7f0e56c73fc5c8f303ffb2260e6
mmulenga <matt.mulengwoo@gmail.com>
2017-08-24 02:20:08 -0600

    Added dark navbar when scrolling. Needs polish.

--- scroll.js
+++ scroll.js
@@ -3,8 +3,11 @@ var home = document.getElementById("homeButton");
 var about = document.getElementById("aboutButton");
 var portfolio = document.getElementById("portfolioButton");
 var contact = document.getElementById("contactButton");
-var mainText= document.getElementById("liveText");
+var mainText = document.getElementById("liveText");
+var nav = document.getElementById("navBar");
 var viewportHeight = window.innerHeight;
+var previousScrollPos = 0;
+var tick = false;
 
 //-------- Functions --------
 $(document).ready(function() {
@@ -28,8 +31,25 @@ $(document).ready(function() {
       window.scrollTo(0, viewportHeight * 3);
     }
 
+    function enableScrollUI(pos) {
+      $(mainText).css("opacity", 1 - $(window).scrollTop() / (viewportHeight / 2.5));
+
+      
+        $(nav).css("background-color", "rgba(45, 45, 45, " + $(window).scrollTop() / viewportHeight + ")");
+        console.log($(nav).css("opacity"));
+
+    }
+
     // Fade the Live in Colour text
-    $(window).scroll(function() {
-      $(mainText).css("opacity", 1 - $(window).scrollTop() / 250);
-    });
+    window.onscroll = function() {
+      if(!tick) {
+        previousScrollPos = window.scrollY;
+
+        window.requestAnimationFrame(function() {
+          enableScrollUI(previousScrollPos);
+          tick = false;
+        });
+      }
+      tick = true;
+    }
 });
