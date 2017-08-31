c60c5a932532e8bb090ebd1e7b2499cb2aa83441
mmulenga <matt.mulengawoo@gmail.com>
2017-08-30 22:15:19 -0600

    Added localStorage for cleaner page reloads.

--- scroll.js
+++ scroll.js
@@ -11,6 +11,36 @@ var tick = false;
 
 //-------- Functions --------
 $(document).ready(function() {
+    if(storageAvailable('localStorage')) {
+      // Check to see if the page was previously scrolled,
+      // if it was set the opacity of the navbar to its
+      // previous state.
+      if(localStorage.getItem("storedScroll")) {
+        var storedScroll = localStorage.getItem("storedScroll");
+      }
+
+      if(storedScroll < 0.8) {
+        $(nav).css("background-color", "rgba(45, 45, 45, " + storedScroll + ")");
+      } else if(storedScroll >= 0.8){
+        $(nav).css("background-color", "rgba(45, 45, 45, 0.8)");
+      }
+
+      // Check to see if the page was previously scrolled,
+      // if it was set the opacity and display of the
+      // main text to its previous state.
+      if(localStorage.getItem("storedMainText")) {
+        var storedMainText = localStorage.getItem("storedMainText");
+      }
+
+      if(storedMainText <= 0) {
+        $(mainText).css("display", "none");
+      } else {
+        $(mainText).css("display", "inline");
+      }
+      
+      $(mainText).css("opacity", storedMainText);
+    }
+
     home.onclick = function() {
       $("html, body").animate({ scrollTop: $("#landingBG").offset().top}, 1000);
     }
@@ -27,9 +57,39 @@ $(document).ready(function() {
       $("html, body").animate({ scrollTop: $("#contactBG").offset().top}, 1000);
     }
 
+    function storageAvailable(type) {
+      try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+      }
+      catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0;
+        }
+      }
+
     function enableScrollUI(pos) {
       var scrollFactor = window.pageYOffset / viewportHeight;
       $(mainText).css("opacity", 1 - Math.sqrt(window.pageYOffset) * 0.1);
+
+      if(storageAvailable('localStorage')) {
+        localStorage.setItem("storedScroll", scrollFactor);
+        localStorage.setItem("storedMainText", $(mainText).css("opacity"));
+      }
+
       if($(mainText).css("opacity") <= 0) {
         $(mainText).css("display", "none");
       } else {
