1a2320bf2c77e20281ed47ccb037bdef427187b0
mmulenga <matt.mulengawoo@gmail.com>
2017-09-03 02:01:20 -0600

    Almost complete mobile response of webpage. Finishing touches on navbar.

--- scroll.js
+++ scroll.js
@@ -5,81 +5,177 @@ var portfolio = document.getElementById("portfolioButton");
 var contact = document.getElementById("contactButton");
 var mainText = document.getElementById("liveText");
 var nav = document.getElementById("navBar");
+var menuBarButton = document.getElementById("mobileBarButton");
 var viewportHeight = window.innerHeight;
 var previousScrollPos = 0;
 var tick = false;
+var menuIsOpen = false;
 
 //-------- Functions --------
 $(document).ready(function() {
-    if(storageAvailable('localStorage')) {
-      // Check to see if the page was previously scrolled,
-      // if it was set the opacity of the navbar to its
-      // previous state.
-      if(localStorage.getItem("storedScroll")) {
-        var storedScroll = localStorage.getItem("storedScroll");
-      }
-
-      if(storedScroll < 0.8) {
-        $(nav).css("background-color", "rgba(45, 45, 45, " + storedScroll + ")");
-      } else if(storedScroll >= 0.8){
-        $(nav).css("background-color", "rgba(45, 45, 45, 0.8)");
-      }
-
-      // Check to see if the page was previously scrolled,
-      // if it was set the opacity and display of the
-      // main text to its previous state.
-      if(localStorage.getItem("storedMainText")) {
-        var storedMainText = localStorage.getItem("storedMainText");
-      }
-
-      if(storedMainText <= 0) {
-        $(mainText).css("display", "none");
-      } else {
-        $(mainText).css("display", "inline");
-      }
-      
-      $(mainText).css("opacity", storedMainText);
+  if(storageAvailable('localStorage')) {
+    // Check to see if the page was previously scrolled,
+    // if it was set the opacity of the navbar to its
+    // previous state.
+    if(localStorage.getItem("storedScroll")) {
+      var storedScroll = localStorage.getItem("storedScroll");
     }
 
-    home.onclick = function() {
-      $("html, body").animate({ scrollTop: $("#landingBG").offset().top}, 1000);
+    if(storedScroll < 0.8) {
+      $(nav).css("background-color", "rgba(45, 45, 45, " + storedScroll + ")");
+    } else if(storedScroll >= 0.8){
+      $(nav).css("background-color", "rgba(45, 45, 45, 0.8)");
     }
 
-    about.onclick = function() {
-      $("html, body").animate({ scrollTop: $("#aboutBG").offset().top}, 1000);
+    // Check to see if the page was previously scrolled,
+    // if it was set the opacity and display of the
+    // main text to its previous state.
+    if(localStorage.getItem("storedMainText")) {
+      var storedMainText = localStorage.getItem("storedMainText");
     }
 
-    portfolio.onclick = function() {
-      $("html, body").animate({ scrollTop: $("#portfolioBG").offset().top}, 1000);
+    if(storedMainText <= 0) {
+      $(mainText).css("display", "none");
+    } else {
+      $(mainText).css("display", "inline");
     }
 
-    contact.onclick = function() {
-      $("html, body").animate({ scrollTop: $("#contactBG").offset().top}, 1000);
-    }
+    $(mainText).css("opacity", storedMainText);
+  }
 
-    function storageAvailable(type) {
-      try {
-        var storage = window[type],
-            x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-      }
-      catch(e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            storage.length !== 0;
+  menuBarButton.onclick = function() {
+    if(menuIsOpen == true) {
+      $(".navbutton").not("#mobileBarButton").css("display", "none");
+      menuIsOpen = false;
+    } else {
+      $(".navbutton").not("#mobileBarButton").css("display", "flex");
+      menuIsOpen = true;
+    }
+  }
+
+  home.onclick = function() {
+    $("html, body").animate({ scrollTop: $("#landingBG").offset().top}, 1000);
+
+    // If on mobile when the button is pressed hide the drop down menu.
+    if(menuIsOpen == true) {
+      $(".navbutton").not("#mobileBarButton").css("display", "none");
+      menuIsOpen = false;
+    }
+  }
+
+  about.onclick = function() {
+    $("html, body").animate({ scrollTop: $("#aboutBG").offset().top}, 1000);
+
+    // If on mobile when the button is pressed hide the drop down menu.
+    if(menuIsOpen == true) {
+      $(".navbutton").not("#mobileBarButton").css("display", "none");
+      menuIsOpen = false;
+    }
+  }
+
+  portfolio.onclick = function() {
+    $("html, body").animate({ scrollTop: $("#portfolioBG").offset().top}, 1000);
+
+    // If on mobile when the button is pressed hide the drop down menu.
+    if(menuIsOpen == true) {
+      $(".navbutton").not("#mobileBarButton").css("display", "none");
+      menuIsOpen = false;
+    }
+  }
+
+  contact.onclick = function() {
+    $("html, body").animate({ scrollTop: $("#contactBG").offset().top}, 1000);
+
+    // If on mobile when the button is pressed hide the drop down menu.
+    if(menuIsOpen == true) {
+      $(".navbutton").not("#mobileBarButton").css("display", "none");
+      menuIsOpen = false;
+    }
+  }
+
+  var optimizedResize = (function() {
+
+    var callbacks = [],
+    running = false;
+
+    // fired on resize event
+    function resize() {
+
+      if (!running) {
+        running = true;
+
+        if (window.requestAnimationFrame) {
+          window.requestAnimationFrame(runCallbacks);
+        } else {
+          setTimeout(runCallbacks, 66);
         }
       }
+
+    }
+
+    // run the actual callbacks
+    function runCallbacks() {
+
+      callbacks.forEach(function(callback) {
+        callback();
+      });
+
+      running = false;
+    }
+
+    // adds callback to loop
+    function addCallback(callback) {
+
+      if (callback) {
+        callbacks.push(callback);
+      }
+
+    }
+
+    return {
+      // public method to add additional callback
+      add: function(callback) {
+        if (!callbacks.length) {
+          window.addEventListener('resize', resize);
+        }
+        addCallback(callback);
+      }
+    }
+  }());
+
+  // start process
+  optimizedResize.add(function() {
+    // Reset the inline css of the navbutton so the media query can be
+    // applied properly on window resize.
+    if (window.matchMedia("(min-width: 800px)").matches) {
+      $(".navbutton").attr("style", "");
+    }
+  });
+
+  // A check to see if local storage is available for the device.
+  function storageAvailable(type) {
+    try {
+      var storage = window[type],
+      x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    }
+    catch(e) {
+      return e instanceof DOMException && (
+        // everything except Firefox
+        e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage.length !== 0;
+      }
+    }
 
     function enableScrollUI(pos) {
       var scrollFactor = window.pageYOffset / viewportHeight;
@@ -113,4 +209,4 @@ $(document).ready(function() {
       }
       tick = true;
     }
-});
+  });
