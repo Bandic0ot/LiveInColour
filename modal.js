2e2b8cdf99e4d79c83c9e5a42d245ddec136053b
mmulenga <matt.mulengawoo@gmail.com>
2018-01-02 03:16:41 -0600

    Updated naming conventions to be in line with AWS bucket standards.

--- modal.js
+++ modal.js
@@ -9,31 +9,31 @@ var close = document.getElementById("closeButton");
 function selectModal(x) {
   switch (x) {
     case 0:
-      image.style.background = "url('images/Angkor Hall.jpg') center/cover no-repeat";
+      image.style.background = "url('images/angkor-hall.jpg') center/cover no-repeat";
       break;
     case 1:
-      image.style.background = "url('images/Pepper Farm.jpg') center/cover no-repeat";
+      image.style.background = "url('images/pepper-farm.jpg') center/cover no-repeat";
       break;
     case 2:
-      image.style.background = "url('images/Look Down.jpg') center/cover no-repeat";
+      image.style.background = "url('images/look-down.jpg') center/cover no-repeat";
       break;
     case 3:
-      image.style.background = "url('images/Moonlit Deck.jpg') center/cover no-repeat";
+      image.style.background = "url('images/moonlit-deck.jpg') center/cover no-repeat";
       break;
     case 4:
-      image.style.background = "url('images/Ships.jpg') center/cover no-repeat";
+      image.style.background = "url('images/ships.jpg') center/cover no-repeat";
       break;
     case 5:
-      image.style.background = "url('images/Candlelight.jpg') center/cover no-repeat";
+      image.style.background = "url('images/candlelight.jpg') center/cover no-repeat";
       break;
     case 6:
-      image.style.background = "url('images/Japan Street.jpg') center/cover no-repeat";
+      image.style.background = "url('images/japan-street.jpg') center/cover no-repeat";
       break;
     case 7:
-      image.style.background = "url('images/Rainy Street.jpg') center/cover no-repeat";
+      image.style.background = "url('images/rainy-street.jpg') center/cover no-repeat";
       break;
     case 8:
-      image.style.background = "url('images/Elephant Embrace.jpg') center/cover no-repeat";
+      image.style.background = "url('images/elephant-embrace.jpg') center/cover no-repeat";
       break;
     default:
       break;
